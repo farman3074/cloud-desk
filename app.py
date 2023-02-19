44d69147bfad0306296dff14e0b9ae2d6f551dfa
farmanlodhi <95118345+farman3074@users.noreply.github.com>
2023-02-19 16:14:18 +0000

    added dashboard page and navbar

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("home.html")
+
+@app.route("/logincheck")
+def check_login():
+  return render_template("dashboard.html")
+
 print(__name__)
 if __name__ == "__main__" :
   app.run(host="0.0.0.0", debug=True)
-  
