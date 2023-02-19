6a4e0dd34061749d196eaa776fcfbf0d33d39854
farmanlodhi <95118345+farman3074@users.noreply.github.com>
2023-02-19 17:58:42 +0000

    added nav bar and main web pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -6,9 +6,35 @@ app = Flask(__name__)
 def hello_world():
     return render_template("home.html")
 
-@app.route("/logincheck")
+@app.route("/logincheck",methods =["POST"])
 def check_login():
-  return render_template("dashboard.html")
+  user = request.form.get("username") 
+  if  user == "farman":
+      return render_template("dashboard.html", title="Dash Board")
+  else:
+    return render_template("home.html")
+
+@app.route("/dashboard")
+def show_dashbaord():
+  return render_template("dashboard.html", title="Dash Board")
+
+
+@app.route("/members")
+def show_members():
+  return render_template("members.html", title="Members")
+
+@app.route("/spaces")
+def show_spaces():
+  return render_template("spaces.html", title="Spaces")
+
+@app.route("/invoicing")
+def show_invoices():
+  return render_template("invoicing.html", title="Invoicing")
+
+@app.route("/reporting")
+def show_reporting():
+  return render_template("reporting.html", title="Reporting")
+
 
 print(__name__)
 if __name__ == "__main__" :
