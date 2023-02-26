fba91229c770ac2967e80dbf30c5040a1b5763dd
farmanlodhi <95118345+farman3074@users.noreply.github.com>
2023-02-26 15:05:49 +0000

    added vew and edit for spaces

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request,jsonify,redirect,url_for
 #import datafile
-from database import load_members_from_db,load_member_from_db, commit_member_to_db
+from database import load_members_from_db,load_member_from_db, commit_member_to_db, load_spaces_from_db, load_space_from_db, commit_space_to_db
 
 app = Flask(__name__)
 
@@ -20,7 +20,6 @@ def check_login():
 @app.route("/dashboard")
 def show_dashbaord():
   return render_template("dashboard.html", title="Dash Board")
-
 
 @app.route("/members")
 def show_members():
@@ -42,11 +41,28 @@ def commit_member():
   query = "update members set name ='" + request.form.get('nameInput') + "', company = '" + request.form.get('companyInput') + "', email ='" + request.form.get('emailInput') + "', phone = '" +  request.form.get('phoneInput') + "', nature = '" + request.form.get('natureInput') + "', address = '" + request.form.get('addressInput') + "' where ID= " + request.form.get('idInput')
   result = commit_member_to_db(query)
   return redirect(f"/viewmember/{request.form.get('idInput')}")
-  
-  
+    
 @app.route("/spaces")
 def show_spaces():
-  return render_template("spaces.html", title="Spaces")
+  spaces = load_spaces_from_db()
+  return render_template("spaces.html", title="Spaces", spaces=spaces)
+
+@app.route("/viewspace/<id>")
+def show_space(id):
+  space = load_space_from_db(id)
+  return render_template("viewspace.html", title="View Space",space=space)
+
+@app.route("/editspace/<id>")
+def edit_space(id):
+  space = load_space_from_db(id)
+  return render_template("editspace.html", title="Edit space",space=space)
+
+@app.route("/commitspace",methods =["POST"])
+def commit_space():
+  query = "update spaces set name ='" + request.form.get('nameInput') + "', type = '" + request.form.get('typeInput') + "', floor ='" + request.form.get('floorInput') + "', seats = " +  request.form.get('seatsInput') + ", area = " + request.form.get('areaInput') + ", isempty = '" + request.form.get('emptyInput') + "' where ID= " + request.form.get('idInput')
+  result = commit_space_to_db(query)
+  return redirect(f"/viewspace/{request.form.get('idInput')}")
+
 
 @app.route("/invoicing")
 def show_invoices():
--- database.py
+++ database.py
@@ -31,4 +31,26 @@ def commit_member_to_db(query):
     return result
     
     
+def load_spaces_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from spaces"))
+    mem_list = result.all()
+    spaces = []
+    for row in mem_list:
+      spaces.append(row._mapping)
+    return spaces
     
+def load_space_from_db(id):
+  with engine.connect() as conn:
+    query = f"select * from spaces where ID = {id}"
+    result = conn.execute(text(query))
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._mapping
+
+def commit_space_to_db(query):
+  with engine.connect() as conn:
+    result = conn.execute(text(query))
+    return result
