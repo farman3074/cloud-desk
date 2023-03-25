9ebe3f4ed10059f51025edd9370ce9ed2f320820
farmanlodhi <95118345+farman3074@users.noreply.github.com>
2023-03-25 18:39:46 +0000

    updated pcode imports

--- pcode.py
+++ pcode.py
@@ -18,7 +18,8 @@
 # Get members whose bookings need to be invoiced
 # SELECT * FROM clouddesk.members where clouddesk.members.ID in (select memberID from clouddesk.bookings where clouddesk.bookings.bookFrom <= '2023-04-01' and clouddesk.bookings.bookto > '2023-03-02') ----dates need to be the current invoice run date
 # Loop for each member and get all valid bookings and then create one invoice lineitem entry for each booking
-
+from datetime import datetime
+from database import commit_query_to_db
 
 def creat_monthly_batch_invoices(startdate,enddate, memberid):
   return
