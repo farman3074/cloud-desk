e3516fb39cd54777c24d6391d0f1dd8266375c46
farmanlodhi <95118345+farman3074@users.noreply.github.com>
2023-03-25 18:28:50 +0000

    added sql query and notes to batch invoicing file

--- pcode.py
+++ pcode.py
@@ -13,5 +13,12 @@
 
 # scans DB and create invoice entries for billing between startdate and enddate for a member
 # returns a list of dictionary entries for invoices created 
+
+#SQL queries
+# Get members whose bookings need to be invoiced
+# SELECT * FROM clouddesk.members where clouddesk.members.ID in (select memberID from clouddesk.bookings where clouddesk.bookings.bookFrom <= '2023-04-01' and clouddesk.bookings.bookto > '2023-03-02') ----dates need to be the current invoice run date
+# Loop for each member and get all valid bookings and then create one invoice lineitem entry for each booking
+
+
 def creat_monthly_batch_invoices(startdate,enddate, memberid):
   return
